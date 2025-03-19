88ec8a5134486c7f88cf939e4eac12c20e12a6b1
jihopark <jihodayo@gmail.com>
2025-03-19 22:08:56 +0900

    feat: 이상, 광기 갯수 연결

--- components/gacha/IdealControl.vue
+++ components/gacha/IdealControl.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="Ideal-controls">
     <button @click="resetStats">초기화</button>
+    <img :src="require('~/assets/images/Ideal.webp')" alt="이상" class="icon" />
     <span>이상[理想]: {{ ideal }}</span>
+    <img
+      :src="require('~/assets/images/Lunacy.webp')"
+      alt="광기"
+      class="icon"
+    />
+    <span>소모된 광기: {{ computedLunacy }}</span>
   </div>
 </template>
 
@@ -20,6 +27,10 @@ export default Vue.extend({
       type: Number,
       default: 0,
     },
+    lunacy: {
+      type: Number,
+      default: 0,
+    },
   },
   methods: {
     ...mapMutations(["resetStatsById"]),
@@ -27,5 +38,21 @@ export default Vue.extend({
       this.resetStatsById(this.id);
     },
   },
+  computed: {
+    computedLunacy(): number {
+      return this.ideal * 130 * -1;
+    },
+  },
 });
 </script>
+
+<style scoped>
+.icon {
+  height: 24px; /* 높이만 지정하여 비율 유지 */
+  width: auto; /* 원본 비율 유지 */
+  image-rendering: -webkit-optimize-contrast; /* Safari/Chrome 최적화 */
+  image-rendering: crisp-edges; /* Firefox 최적화 */
+  -ms-interpolation-mode: nearest-neighbor; /* IE 최적화 */
+  vertical-align: middle;
+}
+</style>
